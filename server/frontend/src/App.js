f4acdcdcf16ecc805e7f255d4ef95378296fe1b4
sapthashree15 <kssapthashree@gmail.com>
2022-05-19 07:36:05 +0000

    changes made from theia lab environment

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,9 +3,18 @@ import React from 'react';
 import HomePage from './homepage.js';
 import ContactUs from './contactus.js';
 
-/*
-Copy the Web Chat code from Watson Assitant and paste it here
-*/
+
+  window.watsonAssistantChatOptions = {
+      integrationID: "f7072ff5-b766-42e8-a555-e9c118e6f703", // The ID of this integration.
+      region: "us-south", // The region your integration is hosted in.
+      serviceInstanceID: "dc230183-8cf4-48ad-ab23-94211c745e6d", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+    document.head.appendChild(t);
+  });
 
 class App extends React.Component {
 
